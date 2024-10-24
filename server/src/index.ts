47e3173b1e9ee52559d3a6c19a7dd28c98e5ef0c
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-10-24 15:22:13 +1100

    setup express-winston for HTTP request logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import swaggerDocs from "./swagger.js"; // ts compiles to js
 import { reportError, getErrorMessage } from "./utils/error.js";
+import { logRequest } from "./middlewares/requestLogger.js";
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(logRequest);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Successful response.");
--- server/src/middlewares/requestLogger.ts
+++ server/src/middlewares/requestLogger.ts
@@ -1,1 +1,16 @@
-//functions for logging Express HTTP requests
+import winston from "winston";
+import expressWinston from "express-winston";
+import { Request, Response } from "express";
+
+export const logRequest = expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({ filename: "logs/requests.log" })
+    ],
+    format: winston.format.json()
+});
