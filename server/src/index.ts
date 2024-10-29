af2acdae70e2056e0b0217923ae76b1617eab6b0
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-10-29 16:56:55 +1100

    init MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,26 @@ import dotenv from "dotenv";
 import swaggerDocs from "./swagger.js"; // ts compiles to js
 import { reportError, getErrorMessage } from "./utils/error.js";
 import { logRequest } from "./middlewares/requestLogger.js";
+import { mongoose }, mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
+
+//connect to mongoDB
+const dbURI: string =
+    "mongodb+srv://" +
+    process.env.MONGO_DB_USER +
+    ":" +
+    process.env.MONGO_DB_PASSWORD +
+    "@vc.7tdrb.mongodb.net/" +
+    process.env.MONGO_DB_COLLECTION_ONE +
+    "?retryWrites=true&w=majority&appName=vc";
+
+mongoose
+    .connect(dbURI)
+    .then((result: typeof mongoose) => console.log("Connected to Mongo DB"))
+    .catch((err: Error) => console.log(err));
 
 app.use(logRequest);
 
---
+++ server/src/models/visualCrossing.ts
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+const Schema: typeof mongoose.Schema = mongoose.Schema;
+
+const visualCrossingSchema = new Schema({});
