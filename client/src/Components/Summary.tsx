c8239f6616cdea6c50387b91d81886c7cb275eae
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-11-04 19:19:13 +1100

    Define own height

--- client/src/Components/Summary.tsx
+++ client/src/Components/Summary.tsx
@@ -4,57 +4,78 @@ const Summary: React.FC = () => {
   return (
     <div
       style={{
-        position: "relative",
-        height: "100%",
+        margin: "auto",
+        width: "339px",
+        height: "150px",
+        border: "1px solid black",
       }}
     >
       <div
         style={{
-          width: "60%",
-          height: "90%",
-          overflow: "auto",
-          margin: "auto",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          bottom: "0",
-          right: "0",
-          border: "1px solid black",
-          backgroundColor: "#D9D9D9",
+          position: "relative",
+          height: "100%",
         }}
       >
         <div
           style={{
-            fontSize: "18px",
-            marginLeft: "auto",
-            marginRight: "auto",
+            width: "60%",
+            height: "90%",
+            overflow: "auto",
+            margin: "auto",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            bottom: "0",
+            right: "0",
+            border: "1px solid black",
+            backgroundColor: "#D9D9D9",
           }}
         >
-          Newcastle, Australia
-        </div>
-        <div
-          style={{
-            fontSize: "14px",
-            marginLeft: "auto",
-            marginRight: "auto",
-          }}
-        >
-          Lon: -32.919, Lat: 151.779
-        </div>
-        <div
-          style={{ fontSize: "56px", marginLeft: "auto", marginRight: "auto" }}
-        >
-          23°
-        </div>
-        <div
-          style={{ fontSize: "14px", marginLeft: "auto", marginRight: "auto" }}
-        >
-          Sunny
-        </div>
-        <div
-          style={{ fontSize: "14px", marginLeft: "auto", marginRight: "auto" }}
-        >
-          L:16° H:26°
+          <div
+            style={{
+              fontSize: "18px",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            Newcastle, Australia
+          </div>
+          <div
+            style={{
+              fontSize: "14px",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            Lon: -32.919, Lat: 151.779
+          </div>
+          <div
+            style={{
+              fontSize: "56px",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            23°
+          </div>
+          <div
+            style={{
+              fontSize: "14px",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            Sunny
+          </div>
+          <div
+            style={{
+              fontSize: "14px",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            L:16° H:26°
+          </div>
         </div>
       </div>
     </div>
