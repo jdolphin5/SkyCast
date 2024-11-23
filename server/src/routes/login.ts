205948e1e0128098f170a33c565eea3776cb51d7
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-11-23 22:09:18 +1100

    salted and hashed password

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -1,17 +1,37 @@
 import express, { Request, Response, Router } from "express";
 import axios from "axios";
 import { reportError, getErrorMessage } from "../utils/error.js";
+import bcrypt from "bcrypt";
 
 export const loginRouter: Router = express.Router({
     mergeParams: true
 });
 
 loginRouter.post("/", async (req: Request, res: Response) => {
-    const username: string = req.params.username;
-    const password: string = req.params.password;
+    const username: string = req.body.username;
+    const password: string = req.body.password;
 
     try {
         console.log("username", username, "password", password);
+
+        const saltRounds: number = 10;
+
+        bcrypt.genSalt(saltRounds, (err, salt: any) => {
+            if (err) {
+                console.log("error generating salt for password");
+                return;
+            }
+
+            bcrypt.hash(password, salt, (err, hash: any) => {
+                if (err) {
+                    console.log("error hashing password");
+                    return;
+                }
+
+                console.log("hashed pw", hash);
+            });
+        });
+
         res.json("login response");
     } catch (error) {
         reportError({ message: getErrorMessage(error) });
