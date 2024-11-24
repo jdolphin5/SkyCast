ec17bb7615397841290639298d732bd6a9211484
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-11-24 17:54:54 +1100

    successfully inserts new user into drizzle/DB

--- server/src/services/login.ts
+++ server/src/services/login.ts
@@ -1,10 +1,24 @@
-import { getErrorMessage } from "../utils/error";
 import bcrypt from "bcrypt";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/mysql2";
+import * as schema from "../db/schema.js";
+import { users } from "../db/schema.js";
 
-export const login = (username: string, password: string) => {
+export const login = async (username: string, password: string) => {
     console.log("username", username, "password", password);
 
+    const databaseUrl = process.env.MYSQL_DATABASE_URL;
+
+    if (!databaseUrl) {
+        throw new Error(
+            "MYSQL_DATABASE_URL is not defined in environment variables."
+        );
+    }
+
+    const db = drizzle(databaseUrl, { schema, mode: "default" });
+
     const saltRounds: number = 10;
+    let hashedPassword = "";
 
     bcrypt.genSalt(saltRounds, (err, salt: any) => {
         if (err) {
@@ -12,13 +26,33 @@ export const login = (username: string, password: string) => {
             return;
         }
 
-        bcrypt.hash(password, salt, (err, hash: any) => {
+        bcrypt.hash(password, salt, async (err, hash: any) => {
             if (err) {
                 console.log("error hashing password");
                 return;
             }
 
             console.log("hashed pw", hash);
+
+            const user = await db.query.users.findFirst({
+                where: (users: { username: any }, { eq: any }: any) =>
+                    eq(users.username, username)
+            });
+
+            if (!user) {
+                const currentDateTime = new Date();
+                console.log("current date/time:", currentDateTime);
+
+                const insertedUser = await db.insert(users).values({
+                    username: username,
+                    password_hash: hash,
+                    last_login_date_time: currentDateTime
+                });
+
+                console.log("user inserted");
+            } else {
+                console.log("user:", user);
+            }
         });
     });
 };
